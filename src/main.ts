78c119c76e05e0fb6bd37b7fb35ff4d1b2986474
Kun <yukun.zk@foxmail.com>
2025-07-18 14:56:38 +0800

    引入了一些基本依赖

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+// 完整引入 Element Plus UI
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+
+app.use(ElementPlus);
+
+// 最后
+app.mount("#app");
