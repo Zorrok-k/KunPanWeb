16d38a6dff555de5ada32a2086cf5833f4e01764
Kun <yukun.zk@foxmail.com>
2025-07-18 19:37:22 +0800

    1.Axios 进行二次封装，确定 Api 封装框架
    2.修改./src 别名为@
    
    3.其他调整

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,12 @@ const app = createApp(App);
 // 完整引入 Element Plus UI
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
-
 app.use(ElementPlus);
+
+// 挂载 Api 到全局
+// import api from '@/apis/index.ts'
+// app.config.globalProperties.$api = api;
+
 
 // 最后
 app.mount("#app");
